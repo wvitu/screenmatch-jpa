be412510ac85089a9da2c312254cc7f5bddb9788
Wanderson Vitor Almeida <153443338+wvitu@users.noreply.github.com>
2025-09-09 19:08:29 -0300

    Categorização adicionada ao front

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -50,4 +50,9 @@ public class SerieController {
     public List<EpisodioDTO> obterTemporadasPorNumero(@PathVariable Long id, @PathVariable Long numero){
         return servico.obterTemporadasPorNumero(id, numero);
     }
+
+    @GetMapping("/categoria/{nomeGenero}")
+    public List<SerieDTO> obterSeriePorCategoria(@PathVariable String nomeGenero) {
+        return servico.obterSeriesPorCategoria(nomeGenero);
+    }
 }
